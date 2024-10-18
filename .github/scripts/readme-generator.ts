20e17755fa1bc02bbedc40e31cfdd823924849f8
servalabs.com <166895751+servalabs@users.noreply.github.com>
2024-10-18 10:50:56 +0530

    u1

--- .github/scripts/readme-generator.ts
+++ .github/scripts/readme-generator.ts
@@ -11,7 +11,7 @@ type App = {
 const getAppsList = async () => {
   const apps: Record<string, App> = {};
   const res = await fetch(
-    "https://api.github.com/repos/bigbeartechworld/big-bear-casaos/contents/Apps"
+    "https://api.github.com/repos/servalabs/app-store/contents/Apps"
   );
 
   if (!res.ok) {
@@ -25,7 +25,7 @@ const getAppsList = async () => {
   for (const app of appNames) {
     if (app === "__tests__") continue;
 
-    const configUrl = `https://raw.githubusercontent.com/bigbeartechworld/big-bear-casaos/master/Apps/${app}/config.json`;
+    const configUrl = `https://raw.githubusercontent.com/servalabs/app-store/master/Apps/${app}/config.json`;
     const configRes = await fetch(configUrl);
 
     if (!configRes.ok) {
--- templates/VIDEO.md
+++
@@ -1,5 +0,0 @@
-This is a todo list of videos that I need to still make for the BigBearYoutube channel.
-
-## Apps
-
-<!appsList>
